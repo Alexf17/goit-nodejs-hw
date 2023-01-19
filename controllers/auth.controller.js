deb1ac0ef8d6fe2f72ebfdbbc21f01a19ce62c3a
Oleksii <achili3f17@gmail.com>
2023-01-19 23:29:45 +0200

    4.1

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,16 @@
+const bcrypt = require("bcrypt");
 const { User } = require("../models/users");
-const { HttpError } = require("./helpers");
+const { HttpError } = require("../helpers");
 
 async function register(req, res, next) {
   const { email, password } = req.body;
+
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
   //   const savedUser = await User.create({ email, password });
   try {
-    const savedUser = await User.create({ email, password });
-    res.status(201).json({ data: { user: savedUser } });
+    const savedUser = await User.create({ email, password: hashedPassword });
+    res.status(201).json({ data: { user: email, id: savedUser._id } });
   } catch (error) {
     if (error.message.includes("E11000 duplicate key")) {
       throw new HttpError(409, "Email in use");
@@ -17,4 +21,23 @@ async function register(req, res, next) {
   //   res.status(201).json({ data:{user: savedUser} });
 }
 
-module.exports = { register };
+async function login(req, res, next) {
+  const { email, password } = req.body;
+
+  const checkUser = await User.findOne({ email });
+  if (!checkUser) {
+    throw new HttpError(401, "email is not valid");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, checkUser.password);
+  if (!isPasswordValid) {
+    throw new HttpError(401, "password is not valid");
+  }
+  res.json({
+    data: {
+      token: "<TOKEN>",
+    },
+  });
+}
+
+module.exports = { register, login };
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const { register } = require("../controllers/auth.controller");
+const { login } = require("../controllers/auth.controller");
 const { tryCatchWrapper } = require("../middlewares/index");
 
 const authRouter = express.Router();
 
 authRouter.post("/register", tryCatchWrapper(register));
+authRouter.post("/login", tryCatchWrapper(login));
 
 module.exports = {
   authRouter,
